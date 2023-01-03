8287bb341025bcde12b56cafbb9ad1fe02d286bf
magometsapraliev@gmail.com <magometsapraliev@gmail.com>
2023-01-03 23:46:22 +0300

    первая программа

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-const app= [1, 4, 6];
+const numberOFfilms = +prompt("сколько фильмов вы посмотрели?", "");
+
+const personalMovieDB = {
+    count : numberOFfilms,
+    movies :{},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+const a = prompt("один из последних фильмов"),
+      b = prompt("оценка"), 
+      c = prompt("один из последних фильмов"),
+      d = prompt("оценка");
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+ 
+console.log(personalMovieDB);
